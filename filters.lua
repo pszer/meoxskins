09d91b3f8a05fd396fe2f5f05d677a033e44ba93
pszer <pszerszynski256@gmail.com>
2025-10-11 17:32:19 +0200

    filters update

---
+++ filters.lua
@@ -0,0 +1,46 @@
+-- filter resources
+--
+
+local filters = {
+
+	defs = {
+		["invert_hsl"] = {
+			filter_implement = "shader",
+			shader = "filter/invert.glsl",
+			params = {},
+			defaults = {},
+		},
+		["hsl_adjust"] = {
+			filter_implement = "shader",
+			shader = "filter/hsl_adjust.glsl",
+			params = {"hueShift","satScale","lumScale","lumCurvedRemap"},
+			defaults = {hueShift=0.0,satScale=1.0,lumScale=1.0,lumCurvedRemap=1.0},
+		},
+	},
+
+	loaded = {
+
+	},
+
+}
+
+function filters.get(name)
+	local def = filters.defs[name]
+	if not def then error("filters.get(): Undefined filter " .. tostring(name)) end
+
+	if filters.loaded[name] then return filters.loaded[name] end
+
+	local filter = require 'filter'
+	local f
+
+	if def.filter_implement == "shader" then
+		f = filter:define_shader_filter(def.shader,def.params,def.defaults)
+	else
+		error("Unknown filter type " .. def.filter_implement)
+	end
+
+	filters.loaded[name] = f
+	return f
+end
+
+return filters
